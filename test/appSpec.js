1aa82ca8daf880c0fa715775107e4cd974aff89a
Anna Górzanowska <anna.gorzanowska@gmail.com>
2018-02-17 12:08:42 +0100

    add test to alive neighbours counter test

--- test/appSpec.js
+++ test/appSpec.js
@@ -1,8 +1,9 @@
 const expect = require("chai").expect;
 const User = require("../src/app_test.js").User;
 
- const Life = require("../src/Life.js");
-//
+const Life = require("../src/Life.js");
+const Game = require("../src/Game.js");
+
 // const assert = require('assert');
 // describe('getName', function () {
 //     describe('gives name', function () {
@@ -13,18 +14,28 @@ const User = require("../src/app_test.js").User;
 // });
 
 describe('Life', function () {
-    let life = new Life(true);
     describe('gives life', function () {
 
-
         it('check life', function () {
-
+            let life = new Life(true);
             expect(life.getAlive()).equal(true);
         });
 
         it('killing', function () {
+            let life = new Life(true);
             life.setAlive(false);
             expect(life.getAlive()).equal(false);
+        });
+
+        it('check number of alive neighbours', function () {
+            let board = [
+                [new Life(true), new Life(false), new Life(false), new Life(true)],
+                [new Life(true), new Life(false), new Life(false), new Life(true)],
+                [new Life(false), new Life(false), new Life(true), new Life(true)],
+                [new Life(true), new Life(false), new Life(true), new Life(true)],
+            ];
+            let game = new Game(board, 4, 4);
+            expect(game.numberOfAliveNeighbours(2, 2)).equal(4);
         });
     });
 });
